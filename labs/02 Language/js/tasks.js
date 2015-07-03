a493ab635d1d1113c536a7f543c614280b938ce6
TonyMak <kwongwai1993@gmail.com>
2015-07-03 20:12:19 +0800

    lab2 task 1
    done~

--- labs/02 Language/js/tasks.js
+++ labs/02 Language/js/tasks.js
@@ -2,30 +2,40 @@
 // This script manages a to-do list.
 
 // Need a global variable:
-var tasks = []; 
+var tasks = [];
+
+function checkDuplicates(array, item) {
+    'use strict';
+    if (array.indexOf(item) === -1) {
+        return true;
+    } else {
+        window.alert('Existed!');
+    }
+}
 
 // Function called when the form is submitted.
 // Function adds a task to the global array.
 function addTask() {
     'use strict';
-
+    var task, output, message, i, count;
     // Get the task:
-    var task = document.getElementById('task');
+    task = document.getElementById('task');
 
     // Reference to where the output goes:
-    var output = document.getElementById('output');
+    output = document.getElementById('output');
     
     // For the output:
-    var message = '';
+    message = '';
 
     if (task.value) {
     
         // Add the item to the array:
-        tasks.push(task.value);
-        
+        if (checkDuplicates(tasks, task.value) === true) {
+            tasks.push(task.value);
+        }
         // Update the page:
         message = '<h2>To-Do</h2><ol>';
-        for (var i = 0, count = tasks.length; i < count; i++) {
+        for (i = 0, count = tasks.length; i < count; i += 1) {
             message += '<li>' + tasks[i] + '</li>';
         }
         message += '</ol>';
@@ -43,4 +53,9 @@ function init() {
     'use strict';
     document.getElementById('theForm').onsubmit = addTask;
 } // End of init() function.
+
 window.onload = init;
+var button;
+
+button = document.getElementById('submit');
+button.onclick = addTask;
